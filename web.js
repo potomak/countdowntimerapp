1c07eb5e25f0c618b6a572f3cb1e516a7af7701f
Giovanni Cappellotto <potomak84@gmail.com>
2011-11-28 21:01:34 +0100

    Add PostgresSQL and Redis example code
    Commented because I'm too lazy to set up development env.

--- web.js
+++ web.js
@@ -1,3 +1,7 @@
+/////////////
+// EXPRESS //
+/////////////
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -10,3 +14,31 @@ var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+//////////////
+// POSTGRES //
+//////////////
+
+// var pg = require('pg');
+// 
+// pg.connect(process.env.DATABASE_URL, function(err, client) {
+//   var query = client.query('SELECT * FROM your_table');
+// 
+//   query.on('row', function(row) {
+//     console.log(JSON.stringify(row));
+//   });
+// });
+
+
+///////////
+// REDIS //
+///////////
+
+// var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+// 
+// redis.set('foo', 'bar');
+// 
+// redis.get('foo', function(err, value) {
+//   console.log('foo is: ' + value);
+// });
